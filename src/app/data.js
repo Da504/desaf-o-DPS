c0fa5a896ed064395e2cd5e1ca6d39b39886e9b0
Da504 <dam342296@gmail.com>
2025-02-26 13:17:03 -0600

    Primer commit - Subiendo proyecto de React

---
+++ src/app/data.js
@@ -0,0 +1,41 @@
+export const data = [
+  {
+    id: 1,
+    title: "Cien años de soledad",
+    price: 100,
+    urlImage: "https://images.penguinrandomhouse.com/cover/9780525562443",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "El señor de los anillos(Trilogía)",
+    price: 190,
+    urlImage:
+      "https://hips.hearstapps.com/hmg-prod/images/up6gihzlfeqzkhifqwcooagozot-66d6dd8e28710.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Cuentos de Barro",
+    price: 30,
+    urlImage:
+      "https://cdn.arcgis.com/sharing/rest/content/items/3536d6d5e14f44078903530c5a163316/resources/MGWVE1PxrPquzjPjMn6Od.jpg?w=400",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Tierra de Infancia",
+    price: 30,
+    urlImage:
+      " https://leolibri.net/image/cache/catalog/2024/febrero%2026%20ev/Lars-TierradeInfancia-927x1000.jpg",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Harry Potter Pack",
+    price: 390,
+    urlImage:
+      "https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113",
+    quantity: 1,
+  },
+];
